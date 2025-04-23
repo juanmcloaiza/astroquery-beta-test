573be3050c93357082b8dfa91cd13507f7b71c91
Juan Manuel Carmona Loaiza <14291849+juanmcloaiza@users.noreply.github.com>
2025-04-23 12:44:20 +0000

    Align with col-op-val astroquery branch

--- install_astroquery.sh
+++ install_astroquery.sh
@@ -1,20 +1,19 @@
 #!/usr/bin/env bash
 
+rm -rf TEST_ESO_ASTROQUERY
 mkdir TEST_ESO_ASTROQUERY
 cd TEST_ESO_ASTROQUERY/
 
 git clone https://github.com/juanmcloaiza/astroquery.git
 cd astroquery
-git checkout TAP
+git checkout colname-op-val
 python -m pip install .
 cd ..
 
 echo ""
 echo " ##### "
 echo ""
+echo ' If the line below reads "ESO TAP url: http://archive.eso.org/tap_obs", everything went ok'
 python -c 'from astroquery.eso import Eso; print(f"ESO TAP url: {Eso()._tap_url()}")'
-echo ""
-echo ' If the line above reads "ESO TAP url: http://archive.eso.org/tap_obs", everything went ok'
-echo ""
 echo ""
 echo " ##### "
