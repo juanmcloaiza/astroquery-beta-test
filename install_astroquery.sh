ab82266978e3b7b0d8394e4979dc5c1891e6378d
Juan Manuel Carmona Loaiza <14291849+juanmcloaiza@users.noreply.github.com>
2025-04-24 15:49:00 +0000

    Update beta test with fail query example

--- install_astroquery.sh
+++ install_astroquery.sh
@@ -8,12 +8,14 @@ git clone https://github.com/juanmcloaiza/astroquery.git
 cd astroquery
 git checkout TAP
 python -m pip install .
+LAST_COMMIT=$(git log --oneline | head -1)
 cd ..
+clear
 
 echo ""
 echo " ##### "
-echo ""
 echo ' If the line below reads "ESO TAP url: http://archive.eso.org/tap_obs", everything went ok'
 python -c 'from astroquery.eso import Eso; print(f"ESO TAP url: {Eso()._tap_url()}")'
 echo ""
+echo " Last commit: $LAST_COMMIT"
 echo " ##### "
