37f980327056cf2391eb2a8a00ecc61958e07a10
Juan Manuel Carmona Loaiza <14291849+juanmcloaiza@users.noreply.github.com>
2025-04-24 17:52:23 +0200

    Allow TAP operators in filter_columns (#3)

--- install_astroquery.sh
+++ install_astroquery.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+rm -rf TEST_ESO_ASTROQUERY
 mkdir TEST_ESO_ASTROQUERY
 cd TEST_ESO_ASTROQUERY/
 
@@ -7,14 +8,14 @@ git clone https://github.com/juanmcloaiza/astroquery.git
 cd astroquery
 git checkout TAP
 python -m pip install .
+LAST_COMMIT=$(git log --oneline | head -1)
 cd ..
+clear
 
 echo ""
 echo " ##### "
-echo ""
+echo ' If the line below reads "ESO TAP url: http://archive.eso.org/tap_obs", everything went ok'
 python -c 'from astroquery.eso import Eso; print(f"ESO TAP url: {Eso()._tap_url()}")'
 echo ""
-echo ' If the line above reads "ESO TAP url: http://archive.eso.org/tap_obs", everything went ok'
-echo ""
-echo ""
+echo " Last commit: $LAST_COMMIT"
 echo " ##### "
